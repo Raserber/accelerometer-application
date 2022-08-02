123b65caa056f4f81b7bf86c2ef91b287733e904
Raserber <loun@orange.fr>
2022-08-02 22:09:46 +0200

    stabilize version

--- src/views/documentation/angulaires/y.vue
+++ src/views/documentation/angulaires/y.vue
@@ -1,6 +1,6 @@
 <template>
   <docs-layout>
-    <iframe src="https://my.spline.design/iphone13angulairey-7e82ae052457593b8279c7193f58a9f4/" frameborder="0" width="100%" height="100%"/>
+    <iframe src="https://accelerometer-application-demos.vercel.app/angular-y.html" frameborder="0" width="100%" height="100%"/>
   </docs-layout>
 </template>
 
